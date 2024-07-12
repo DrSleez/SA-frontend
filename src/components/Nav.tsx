e1a93c8d7aac6816a124d82f27710dce0afc2dbc
DrSleez <carl.weiss@trepdeck.de>
2024-07-12 21:06:47 +0200

    anpassungen navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
-import { AppShell, Burger, Anchor, Group } from "@mantine/core";
+import {
+  AppShell,
+  Burger,
+  Anchor,
+  Group,
+  Stack,
+  Center,
+  Title,
+  Button,
+} from "@mantine/core";
+import { FaBasketShopping } from "react-icons/fa6";
 import { Outlet } from "react-router-dom";
 
 export default function Nav() {
   const [opened, setOpened] = useState(false);
 
+  function handleCartClick() {}
+
   return (
     <AppShell
-      header={{ height: 60, offset: true }}
+      header={{ height: 60 }}
       navbar={{
         width: 300,
         breakpoint: "sm",
@@ -16,14 +28,31 @@ export default function Nav() {
       padding="md"
     >
       <AppShell.Header>
-        <Group h="100%" px="md">
-          <Burger opened={opened} onClick={() => setOpened((o) => !o)} />
+        <Group h="100%" px="md" justify="space-between">
+          <Group>
+            <Burger opened={opened} onClick={() => setOpened((o) => !o)} />
+          </Group>
+          <Group>
+            {" "}
+            <Title>Software Moftware</Title>
+          </Group>
+          <Group>
+            <Button
+              variant="outline"
+              style={{ marginLeft: "3em" }}
+              onClick={() => handleCartClick()}
+            >
+              <FaBasketShopping size="1.5rem" />
+            </Button>
+          </Group>
         </Group>
       </AppShell.Header>
       <AppShell.Navbar>
-        <Anchor>Home</Anchor>
-        <Anchor>Features</Anchor>
-        <Anchor>Pricing</Anchor>
+        <Stack w="100%" px="md" p="auto">
+          <Anchor>Home</Anchor>
+          <Anchor>Features</Anchor>
+          <Anchor>Pricing</Anchor>
+        </Stack>
       </AppShell.Navbar>
       <AppShell.Main>
         <Outlet />
