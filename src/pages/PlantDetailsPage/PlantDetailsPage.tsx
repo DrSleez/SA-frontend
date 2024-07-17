1b8a7fa14def9a449c8d6be7cb9ca18b9f345272
DrSleez <carl.weiss@trepdeck.de>
2024-07-17 14:52:05 +0200

    add more functions to basket context

--- src/pages/PlantDetailsPage/PlantDetailsPage.tsx
+++ src/pages/PlantDetailsPage/PlantDetailsPage.tsx
@@ -2,7 +2,18 @@ import { useParams } from "react-router-dom";
 import usePlant from "../../hooks/usePlant";
 import { UUID } from "crypto";
 import LoadingPage from "../LoadingPage/LoadingPage";
-import { Container, Title, Text, Image, Grid, Loader } from "@mantine/core";
+import {
+  Container,
+  Title,
+  Text,
+  Image,
+  Grid,
+  Loader,
+  Button,
+  Flex,
+} from "@mantine/core";
+import useIsAuthenticated from "react-auth-kit/hooks/useIsAuthenticated";
+import { useBasket } from "../../context/BasketContext";
 
 function useRequiredParams<T extends Record<string, any>>() {
   const params = useParams<T>();
@@ -12,6 +23,14 @@ function useRequiredParams<T extends Record<string, any>>() {
 export default function PlantDetailsPage() {
   const { id } = useRequiredParams<{ id: UUID }>();
   const query = usePlant(id);
+  const isAuthenticated = useIsAuthenticated();
+  const {
+    increaseBasketQuantity,
+    decreaseBasketQuantity,
+    removeFromBasket,
+    getItemQuantity,
+  } = useBasket();
+  const quantity = getItemQuantity(id);
 
   if (query.isPending) {
     return <LoadingPage />;
@@ -28,13 +47,13 @@ export default function PlantDetailsPage() {
   return (
     <Container fluid>
       <Grid>
-        <Grid.Col span={6}>
-          <Image
-            src={query.data.imageLink}
-            alt={query.data.name}
-            fallbackSrc="https://placehold.co/600x400?text=Placeholder"
-          />
-        </Grid.Col>
+        <Image
+          src={query.data.imageLink}
+          alt={query.data.name}
+          fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+          h="500"
+          w="100%"
+        />
         <Grid.Col span={6}>
           <Title>{query.data.name}</Title>
           <Text color="dimmed" size="lg" style={{ marginBottom: "1rem" }}>
@@ -53,6 +72,53 @@ export default function PlantDetailsPage() {
           <Text>
             <strong>Beschreibung:</strong> {query.data.description}
           </Text>
+          {isAuthenticated ? (
+            <>
+              {quantity === 0 ? (
+                <>
+                  <Flex justify={{ sm: "center" }} align="center">
+                    <Button onClick={() => increaseBasketQuantity(id)}>
+                      Add to Basket
+                    </Button>
+                  </Flex>
+                </>
+              ) : (
+                <>
+                  <Flex
+                    gap={{ base: "sm", sm: "lg" }}
+                    justify={{ sm: "center" }}
+                    mb={3}
+                    align="center"
+                  >
+                    <Button
+                      justify="center"
+                      size="sm"
+                      onClick={() => decreaseBasketQuantity(id)}
+                    >
+                      -
+                    </Button>
+                    <span>{quantity}</span>
+                    <Button
+                      justify="center"
+                      size="sm"
+                      onClick={() => increaseBasketQuantity(id)}
+                    >
+                      +
+                    </Button>
+                  </Flex>
+                  <Flex mt={3} justify={{ sm: "center" }} align="center">
+                    <Button
+                      color="red"
+                      size="sm"
+                      onClick={() => removeFromBasket(id)}
+                    >
+                      Remove from Basket
+                    </Button>
+                  </Flex>
+                </>
+              )}
+            </>
+          ) : null}
         </Grid.Col>
       </Grid>
     </Container>
